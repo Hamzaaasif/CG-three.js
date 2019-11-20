82754dc63adebf4d94444fb1f06036e7b9dbe543
Hamza Asif <hamzaasif0987@gmail.com>
2019-11-20 12:41:38 +0500

    working

--- src/index.js
+++ src/index.js
@@ -9,11 +9,18 @@ var scene = new THREE.Scene();
 
 // Create a basic perspective camera
 
-var prescamera = new THREE.PerspectiveCamera(-10 ,10 ,-10 , 10 , 0.1 , 1000 ); //3D camera
-var orthocamera = new THREE.OrthographicCamera(-10 ,10 ,-10 , 10 , 0.1 , 1000 );
+var prescamera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 0.1, 1000 ); //3D 
+prescamera.position.set(0,8,5);
+prescamera.lookAt(new THREE.Vector3(0,0,5));
+
+var orthocamera = new THREE.OrthographicCamera(-10 ,10 ,10 , -10 , 0.1 , 1000 );
+
 //var camera = new THREE.OrthographicCamera(-5 ,5 ,-5 , 5 , 0.1 , 1000 ); //2D camera
-orthocamera.position.z = 4;
-prescamera.position.z = 4;
+orthocamera.position.z=4;
+//orthocamera.position.set(0,10,0);
+//orthocamera.lookAt(new THREE.Vector3(0,5,5));
+//orthocamera.lookAt(new THREE.Vector3(0,8,-10));
+//prescamera.position.z = 4;
 
 // Create a renderer with Antialiasing
 var renderer = new THREE.WebGLRenderer({antialias:true});
@@ -45,7 +52,7 @@ for(var i=-size ; i<=size ; i+=step)
   geometrygrid.vertices.push(new THREE.Vector3(-size , i ,0 ));
   geometrygrid.vertices.push(new THREE.Vector3(size , i ,0 ));
    geometrygrid.vertices.push(new THREE.Vector3(i, -size  ,0 ));
-   geometrygrid.vertices.push(new THREE.Vector3(i, size , 0 ));
+   geometrygrid.vertices.push(new THREE.Vector3(i, size, 0 ));
 }
 
 var line = new THREE.LineSegments( geometrygrid, materialgrid );
@@ -62,14 +69,14 @@ var line = new THREE.LineSegments( geometrygrid, materialgrid );
 var geometrytri = new THREE.Geometry();
 var materialtri = new THREE.MeshBasicMaterial({color: "#433F81" , opacity: 0.2 });
 
-geometrytri.vertices.push (new THREE.Vector3(-2 , 0, 0 ));
-geometrytri.vertices.push (new THREE.Vector3(0 , 0, 0 ));
-geometrytri.vertices.push (new THREE.Vector3(0 , 2, 0 ));
+geometrytri.vertices.push (new THREE.Vector3(-3 , 3, 0 ));
+geometrytri.vertices.push (new THREE.Vector3(0 , 3, 0 ));
+geometrytri.vertices.push (new THREE.Vector3(0 , 5, 0 ));
 
-geometrytri.faces.push (new THREE.Face3(0 , 2, 1 ));
+geometrytri.faces.push (new THREE.Face3(0 , 1, 2 ));
 geometrytri.computeFaceNormals();
 var triangle = new THREE.Mesh(geometrytri, materialtri);
-triangle.position.set(0.0 , 0.0 , 0.0);
+//triangle.position.set(0.0 , 0.0 , 0.0);
 
 
 // Add cube to Scene
